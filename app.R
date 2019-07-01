a5334ea40b85537620d5935366d5597ee9d9210b
domrussel <domrussel@gmail.com>
2019-07-01 19:43:06 -0400

    commit all

---
+++ app.R
@@ -0,0 +1,119 @@
+source("func.R")
+
+###########################################
+
+########## UI #############
+ui <- fluidPage(
+  
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
+  
+  mobileDetect('isMobile'),
+  
+  # Application title
+  tags$div(class = "title", "The Identity Generator"),
+  
+  tags$div(
+    class = "body",
+    
+    tags$br(),
+    uiOutput("identity"),
+    
+    # Button
+    actionButton("generate", "Generate an identity"),
+    textOutput("the_counter"),
+    tags$br(),
+    
+    # Global Graph - Desktop
+    conditionalPanel("input.isMobile == false",
+      plotlyOutput("global_plot")
+    ),
+    
+    tags$br(),
+    
+    # Global Graph - Mobile
+    conditionalPanel("input.isMobile == true",
+      tags$label(
+        tags$input(id="show_global", type="checkbox"),
+        tags$span(id="check_text", "Show how this compares to the global distribution")
+      ),
+      conditionalPanel("input.show_global == true",
+                       plotOutput("global_plot.mobile"))
+    ),
+    tags$label(
+      tags$input(id="show_info", type ="checkbox"),
+      tags$span(id="check_text2", "Show me your math and assumptions")
+    ),
+    tags$br(),
+    conditionalPanel("input.show_info == true",
+                     tags$p(id="information",
+                            "This is app version 0.3. It attempts to generate a random person and their income using the World Bank income and population data.
+                            As is widely noted - income data has large uncertainty. Income data is collected in many different ways in different countries and can represent
+                            different things. For simplicity, we rely entirely on the World Bank data. This is not intended to be a perfectly technical analysis of global incomes.
+                            To generate our estimates, we first sample a country, weighted by population. Then sample the in-country quantile from a uniform distribution.
+                            We estimate an individual at that quantile's income by plugging this uniform random variable into the CDF of the country's Lorenz Curve
+                            and subtracting that value by the value of the cdf of the lorenz curve at quantile-(1/pop). The Lorenz curve CDF is
+                            estimated by a Pareto distribution with parameters based-on the country's median income and gini coefficient. The use of the Pareto distribution is informed by:",
+                            tags$a(href="http://www.cgeh.nl/sites/default/files/WorkingPapers/CGEH.WP_.No1_.Vanzandenetal.jan2011_0.pdf",
+                                   "http://www.cgeh.nl/sites/default/files/WorkingPapers/CGEH.WP_.No1_.Vanzandenetal.jan2011_0.pdf"))
+    )
+  )
+)
+
+#### Server ####
+server <- function(input, output) {
+  
+  values <- reactiveValues()
+  values$the_counter <- 0
+  values$country <- NULL
+  values$income <- -100000
+  
+  observeEvent(input$generate, {
+    ident <- get_person()
+    values$the_counter <- values$the_counter + 1
+    values$country <- ident[[1]]
+    values$income <- ident[[2]]
+  })
+  
+  output$identity <- renderUI({
+    if(is.null(values$country)) ""
+    else{
+      tags$div("You are from", tags$b(values$country), 
+             "and your annual income (purchasing-power-adjusted to US$) is", 
+             tags$b(dollar_format()(round(values$income))))
+    }
+  })
+  
+  output$the_counter <- renderText({
+    paste("Number of identities generated =", values$the_counter)
+  })
+  
+  output$global_plot <- renderPlotly({
+    if(values$income < 70000){
+      global_plot %>%
+        layout(shapes = list(vline(values$income)),
+               xaxis = list(title = 'Annual Income (PPP Adjusted to US$)', range=c(0,70000)))
+    }
+    else{
+      global_plot %>%
+        layout(shapes = list(vline(values$income)),
+               xaxis = list(title = 'Annual Income (PPP Adjusted to US$)', range=c(0,values$income+5000)))
+    }
+  })
+  
+  output$global_plot.mobile <- renderPlot({
+    if(values$income < 70000){
+      global_plot.mobile + 
+        geom_vline(xintercept = values$income, color='black', linetype = 'dashed', size=1)
+    }
+    else{
+      global_plot.mobile + 
+        geom_vline(xintercept = values$income, color='black', linetype = 'dashed', size=1) +
+        scale_x_continuous(labels = dollar, limits = c(0,values$income))
+    }
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ func.R
@@ -0,0 +1,167 @@
+library(readr)
+library(ggplot2)
+library(scales)
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(ggthemes)
+library(plotly)
+
+# A function to get the most recent year avaliable from WB data
+get_most_recent <- function(dt){
+  dt <- dt %>% 
+    select(-one_of(c("Country Code", "Indicator Name", "Indicator Code")), Country='Country Name') %>% 
+    gather("Year", "Value", -"Country") %>% 
+    na.omit() %>% 
+    group_by(Country) %>% 
+    filter(Year == max(Year)) %>% 
+    ungroup() %>% 
+    right_join(select(dt, Country='Country Name'), by='Country') 
+  dt$Value = as.numeric(dt$Value)
+  dt
+}
+
+###### import and clean the WB data #######
+get_dat <- function(){
+  population <- read_csv("data/population_world_bank.csv", skip=4)
+  income <- read_csv("data/income_world_bank.csv", skip=4) #GDP per capita, PPP (current international $)
+  gini <- read_csv("data/gini_world_bank.csv", skip=4) #GINI index (World Bank estimate)
+  rows_to_remove <- read_csv("data/rows_to_remove.csv")$to_remove
+  
+  # first get the most recent avaliable year
+  population <- get_most_recent(population)
+  income <- get_most_recent(income)
+  gini <- get_most_recent(gini)
+  
+  # now combine the data step by step
+  dat <- population %>% 
+    select(country = 'Country', population = 'Value')
+  dat <- income %>% 
+    select(country = 'Country', income = 'Value') %>% 
+    inner_join(dat)
+  dat <- gini %>% 
+    select(country = 'Country', gini = 'Value') %>% 
+    inner_join(dat)
+  
+  # filter out the world bank summary rows (i.e. World total, Central Asia total)
+  dat <- filter(dat, !(country %in% rows_to_remove))
+  # 217 counties
+  
+  # drop countries with NA income ~ 1% of global population
+  # (sum(dat$population) - sum(filter(dat, !is.na(income))$population)) / sum(dat$population)
+  dat <- dat %>%
+    filter(!is.na(income))
+  # 194 countries
+  
+  # there are 33/194 countries without gini data
+  # replace the na's with the median of the knowns
+  med_gini <- median(dat$gini[!is.na(dat$gini)])
+  dat$gini[is.na(dat$gini)] <- med_gini
+  dat$gini <- dat$gini/100
+  
+  dat
+}
+
+# see the paper below
+# http://www.cgeh.nl/sites/default/files/WorkingPapers/CGEH.WP_.No1_.Vanzandenetal.jan2011_0.pdf
+lorenz_cdf <- function(p, gini){
+  sigma <- sqrt(2)*qnorm((1+gini)/2)
+  pnorm(qnorm(p) - sigma)
+}
+
+# get an income from a country
+get_inc <- function(nation, n=1){
+  inc <- filter(dat, country == nation)$income
+  pop <- filter(dat, country == nation)$population
+  g <- filter(dat, country == nation)$gini
+  
+  total_inc <- inc*pop # the country's total income
+  person_pct <- 1/pop # percent of total pop each person is
+  U <- runif(n)
+  while(sum(U > person_pct) < n){
+    U[U <= person_pct] <- runif(1)
+  }
+  # the difference btwn the percentile sampled and one person down; this is percent of total income for one person
+  (lorenz_cdf(U, g) - lorenz_cdf(U - 1/pop, g)) * total_inc 
+}
+
+# final function
+get_person <- function(){
+  nation <- sample_n(dat, size=1, weight=population)$country
+  inc <- get_inc(nation)
+  list(nation, inc)
+}
+
+# actually store the dat
+dat <- get_dat()
+
+
+# save_dat5 <- data.frame(matrix(nrow=10000, ncol=1))
+# colnames(save_dat5) <- c("income")
+# for(i in 1:10000){
+#   save_dat5[i,1] <- get_person()[2]
+# }
+# 
+# final_dist <- rbind(
+#   save_dat, save_dat2, save_dat3, save_dat4, save_dat5
+# )
+# 
+# write_csv(final_dist, "fifty-k_points.csv")
+
+# plot_dat <- 
+#   read_csv("fifty-k_points.csv") %>% 
+#   arrange(income)
+# saveRDS(plot_dat, "global_50k_dat.Rdata")
+
+vline <- function(x = 0, color = 'rgb(10, 10, 10)') {
+  list(
+    type = "line", 
+    y0 = 0, 
+    y1 = 1, 
+    yref = "paper",
+    x0 = x, 
+    x1 = x, 
+    line = list(color = color, dash = 'dashdot')
+  )
+}
+
+plot_dat <- readRDS("global_50k_dat.Rdata")
+
+dsty <- density(plot_dat$income, adjust=0.001, n=1024)
+final_dist_samp <- plot_dat[seq(1, nrow(plot_dat), nrow(plot_dat)/100),]
+dat_dnsty <- approx(dsty$x, dsty$y, final_dist_samp$income)
+
+dat_dnsty$quant <- c(0:99)
+dat_dnsty$text <- paste0(dollar_format()(round(dat_dnsty$x, -2)), "\n", dat_dnsty$quant, "%tile income")
+
+m <- list(
+  l = 50,
+  r = 150,
+  b = 50,
+  t = 50,
+  pad = 5
+)
+
+global_plot <- plot_ly(x = ~dat_dnsty$x, y = ~dat_dnsty$y, text = ~dat_dnsty$text,
+                        hoverinfo = "text", type = 'scatter', mode = 'lines', fill = 'tozeroy') %>%
+                layout(title = "Global Number of People by Income",
+                        xaxis = list(title = 'Annual Income (PPP Adjusted to US$)', range=c(0,70000)),
+                        yaxis = list(title = 'People', showticklabels = FALSE, fixedrange=TRUE),
+                        margin = m)
+
+global_plot.mobile <- ggplot(plot_dat, aes(x=income)) + geom_density(color = 'white', fill='#66a3ff') +
+  theme_hc() +
+  xlab('Annual Income (PPP Adjusted to US$)') +
+  ylab('People') +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())+
+  scale_x_continuous(labels = dollar, limits = c(0,90000))
+
+mobileDetect <- function(inputId, value = 0) {
+  tagList(
+    singleton(tags$head(tags$script(src = "js/mobile.js"))),
+    tags$input(id = inputId,
+               class = "mobile-element",
+               type = "hidden")
+  )
+}
